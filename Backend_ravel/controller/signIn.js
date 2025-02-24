0241f74ff7095e6ed7bafb31d11c1dccd110805a
Anuj <anujguptaaj123@gmail.com>
2025-02-24 22:22:29 +0530

    bcrypt

--- Backend_ravel/controller/signIn.js
+++ Backend_ravel/controller/signIn.js
@@ -1,5 +1,5 @@
 import User from "../model/user.js";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import { validationResult } from "express-validator";
 
