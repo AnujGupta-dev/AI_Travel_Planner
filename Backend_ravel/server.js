f217a3088639cfe0ae9f3a909d92430be8f0e91b
Anuj <anujguptaaj123@gmail.com>
2025-05-11 20:56:02 +0530

    backend

--- Backend_ravel/server.js
+++ Backend_ravel/server.js
@@ -2,14 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv'
 import dbconnect from './config/database.js'
 import router from './routes/userRoutes.js';
+import cors from 'cors'
 
 const app = express();
 
 dotenv.config()
 const PORT = process.env.PORT || 4000 ;
 
+app.use(cors)
+app.use(cors({
+    origin: 'https://ai-travel-planner-2kla.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}))
+
 app.use(express.json());
 app.use("/api" , router)
+
 
 dbconnect();
 
