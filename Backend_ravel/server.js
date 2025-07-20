16b87a51b6b71dc7039dde86406ae7f5648f1cd7
Anuj <anujguptaaj123@gmail.com>
2025-07-20 09:36:26 +0530

    server js modify

--- Backend_ravel/server.js
+++ Backend_ravel/server.js
@@ -9,13 +9,12 @@ const app = express();
 dotenv.config()
 const PORT = process.env.PORT || 4000 ;
 
-app.use(cors({
-    origin: 'https://ai-trip-planner-6c32.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-}))
+app.use(cors())
 
 app.use(express.json());
+
+app.use(express.static('build')) ;
+
 app.use("/api" , router)
 
 
